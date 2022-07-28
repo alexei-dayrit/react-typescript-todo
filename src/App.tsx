262fed082a776ec810d38968f4aa2517a81559fe
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-28 22:55:54 +0000

    Created components folder with InputField component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1 className='heading'>WhaToDo</h1>
+      <InputField />
     </div>
   );
 }
