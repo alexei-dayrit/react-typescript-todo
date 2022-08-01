269c3488d0a919a9fefd17472a22738ccbb3cb44
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-08-01 23:14:08 +0000

    imported all necessary react-icons.

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import { Todo } from '../model'
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 
 interface Props {
   todo: Todo;
-  key: number;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoItem = ({todo, key, todos, setTodos}:Props) => {
+const TodoItem = ({todo, todos, setTodos}:Props) => {
   return (
-    <div>
-
-    </div>
-  )
+    <form className="single-todo">
+      <div className="single-todo-text">{todo.todo}</div>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon">
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
 }
 
 export default TodoItem
